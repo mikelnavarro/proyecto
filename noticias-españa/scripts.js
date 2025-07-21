71e1427f5fb4b15d708a2f588e5e4504de0c3f63
Mikel Navarro <mikelnavarrolopez@hotmail.com>
2025-07-21 13:32:28 +0200

    Actualizacion News Diario Actualización de desarrollo

--- noticias-españa/scripts.js
+++
@@ -1,8 +0,0 @@
-    function mostrarInfo() {
-        var info = document.getElementById("infoExtra");
-        if (info.classList.contains("hidden")) {
-            info.classList.remove("hidden");
-        } else {
-            info.classList.add("hidden");
-        }
-    }
